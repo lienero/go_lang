3e3ef4790124b79d6b9b3554c9686e015508fe68
lienero <lienero@naver.com>
2024-02-01 16:26:37 +0900

    재입력 기능 테스트(입력 스트림 비우기)

--- scan/scan.go
+++ scan/scan.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"bufio" // io를 담당하는 패키지
+	"fmt"
+	"os" // 표준 입출력 등을 가지고 있는 패키지
+)
 
 func main() {
+	// 표준 입력을 ㅇ릭는 객체
+	stdin := bufio.	NewReader(os.Stdin)
 	// 값을 저장할 변수 선언
 	var a int
 	var b int
@@ -15,6 +21,16 @@ func main() {
 	// n, err := fmt.Scanf("%d %d\n", &a, &b)
 	// 엔터 키로 입력을 종료해야함
 	n, err := fmt.Scanln(&a, &b)
+	if err != nil {
+		fmt.Println(n, err)
+		// 표준 입력 스트링 지우기
+		stdin.ReadString('\n')
+	} else {
+		fmt.Println(n, a, b)
+	}
+
+	// 다시 입력받기
+	n, err = fmt.Scanln(&a, &b)
 	if err != nil {
 		fmt.Println(n, err)
 	} else {
